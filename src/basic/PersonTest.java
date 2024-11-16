66cb769282395211493839e6f59a8812bfb3c03b
kimsaemjava <heaves@hanmail.net>
2024-11-16 13:30:11 +0900

    첫 번째 작업 완료   - 템플릿완성   - 자바스크립트 작업완료   - DTO작업완료

---
+++ src/basic/PersonTest.java
@@ -0,0 +1,25 @@
+package basic;
+
+
+import java.util.ArrayList;
+
+
+public class PersonTest {
+    public static  void main(String[] args) {
+        Person person = new Person("석진",30,"서울");
+        //ctrl+alt+v하면 자동으로 변수가 선언된다.
+        String name = person.getName();
+        ArrayList<String> list = new  ArrayList();
+
+        //필요없는 경우
+        //ctrl+alt+o
+
+        int result = 1900+100;
+        result = 1900+100;
+        result = 1900+100;
+        result = 1900+100;
+        result = 1900+100;
+        result = 1900+100;
+
+    }
+}
---
+++ src/com/kim2/test/Test.java
@@ -0,0 +1,4 @@
+package com.kim2.test;
+
+public class Test {
+}
